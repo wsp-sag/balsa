983fb701ac5ebf0899dd0e1539d3ad73d361d23e
PeterKucirek <pkucirek@pbworld.com>
2017-03-15 18:04:40 -0400

    Added `scribe` module for simplifying logging of model results

--- balsa/__init__.py
+++ balsa/__init__.py
@@ -3,3 +3,4 @@ import balsa.cheval
 import balsa.matrices
 import balsa.pandas_utils
 import balsa.utils
+import balsa.scribe
---
+++ balsa/scribe/__init__.py
@@ -0,0 +1,1 @@
+from .scribe import get_model_logger, get_root_logger, remove_jupyter_handler, log_to_file
