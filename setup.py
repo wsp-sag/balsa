42df7fc6ba573c673a83346ae86d7c0dd052de41
PeterKucirek <pkucirek@pbworld.com>
2017-06-19 14:13:38 -0400

    Moved requirements to a separate file for use in Conda install scripts

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
 from setuptools import setup, find_packages
+from os import path
+
+
+def process_line(line):
+    if '#' in line:
+        first_comment_char = line.index('#')
+        line = line[:first_comment_char]
+    return line.strip()
+
+fp = path.join(path.dirname(__file__), "requirements.txt")
+with open(fp) as reader:
+    installation_requirements = [process_line(line) for line in reader if not line.startswith('#')]
 
 setup(
     name='balsa',
-    version='0.2',
-    requires=[
-        'pandas',
-        'numpy',
-        'numba',
-        'numexpr',
-        'astor',
-        'six'
-    ],
+    version='0.3',
+    requires=installation_requirements,
     packages=find_packages()
 )
