10d0eaa27bdd6b5d81eb02a60f67e9812501b4a5
PeterKucirek <pkucirek@pbworld.com>
2017-06-26 12:34:08 -0400

    Created a recipe for use by conda install

--- setup.py
+++ setup.py
@@ -1,20 +1,7 @@
 from setuptools import setup, find_packages
-from os import path
-
-
-def process_line(line):
-    if '#' in line:
-        first_comment_char = line.index('#')
-        line = line[:first_comment_char]
-    return line.strip()
-
-fp = path.join(path.dirname(__file__), "requirements.txt")
-with open(fp) as reader:
-    installation_requirements = [process_line(line) for line in reader if not line.startswith('#')]
 
 setup(
     name='balsa',
-    version='0.3',
-    requires=installation_requirements,
+    version='0.3.0',
     packages=find_packages()
 )
