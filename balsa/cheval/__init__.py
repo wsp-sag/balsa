9b838567310b579df0e3ce68c3a3230f07e9ffcc
PeterKucirek <pkucirek@pbworld.com>
2017-08-08 11:40:53 -0400

    Added sample_from_weights to the global scope since it's used as a standalone utility function

--- balsa/cheval/__init__.py
+++ balsa/cheval/__init__.py
@@ -1,3 +1,3 @@
-from .api import ChoiceModel
+from .api import ChoiceModel, sample_from_weights
 from .ldf import LinkedDataFrame, LinkageSpecificationError
 from .scope import ScopeOrientationError, InconsistentUsageError, UnsupportedSyntaxError
