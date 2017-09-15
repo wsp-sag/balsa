a4f145d1db94054570cddf9e749eec5dec38bc97
PeterKucirek <pkucirek@pbworld.com>
2017-09-15 12:58:41 -0400

    Added required libraries, just to prevent PyCharm from complaining. The real dependencies are in the meta.yaml file

--- setup.py
+++ setup.py
@@ -3,5 +3,11 @@ from setuptools import setup, find_packages
 setup(
     name='balsa',
     version='0.5.0',
-    packages=find_packages()
+    packages=find_packages(),
+    requires=[
+        'pandas',
+        'numpy',
+        'astor',
+        'numba'
+    ]
 )
