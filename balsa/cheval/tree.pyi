a71f8cfc8b60134edb4708ebf6e88960e2e96d51
PeterKucirek <pkucirek@pbworld.com>
2017-09-21 16:58:06 -0400

    Improved type hinting for ChoiceTree

--- balsa/cheval/tree.pyi
+++ balsa/cheval/tree.pyi
@@ -1,8 +1,14 @@
-from typing import Iterable, List, Tuple
+from typing import Iterable, List, Tuple, Any, Set, Union, Dict
 import pandas as pd
 import numpy as np
 
 class _ChoiceNode:
+
+    _root: 'ChoiceTree'
+    _parent: Union['ChoiceTree', '_ChoiceNode']
+    _name: str
+    _level: int
+    _children: Set['_ChoiceNode']
 
     def __init__(self, *args):
         pass
@@ -34,6 +40,9 @@ class _ChoiceNode:
 
 class ChoiceTree:
 
+    _children: Set[_ChoiceNode]
+    _all_nodes: Dict[str, _ChoiceNode]
+
     def __init__(self, *args):
         pass
 
@@ -43,17 +52,21 @@ class ChoiceTree:
     def children(self) -> Iterable['_ChoiceNode']:
         pass
 
+    @property
     def node_index(self) -> pd.Index:
         pass
 
     def add_node(self, name: str, logsum_scale: float=1.0) -> _ChoiceNode:
         pass
 
-    def add_nodes(self, names: List[str], logsum_scales: List[str]=None) -> List[_ChoiceNode]:
+    def add_nodes(self, names: List[str], logsum_scales: List[float]=None) -> List[_ChoiceNode]:
         pass
 
     def remove_node(self, name: str):
         pass
 
     def flatten(self) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+        pass
+
+    def _root_add(self, parent, new_name, logsum_scale, level) -> _ChoiceNode:
         pass
