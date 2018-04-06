9326cd873d20cd2a5623f2f47cbde36b292a8450
PeterKucirek <pkucirek@pbworld.com>
2018-04-06 17:34:17 -0400

    Added new logging module to replace the old "scribe" module, which will be phased out.

---
+++ balsa/logging/__init__.py
@@ -0,0 +1,2 @@
+from .logging import (exclude_console_level, get_model_logger, include_console_level, init_root, log_to_file,
+                      set_console_format, ModelLogger, RangeFilter, SetFilter, SwitchFormatter)
