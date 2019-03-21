296a10675a700663170987a63cb116e1a48ebac4
PeterKucirek <pkucirek@pbworld.com>
2019-03-21 14:32:49 -0400

    Updated dependencies

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='balsa',
-    version='0.6.1',
+    version='1.0',
     packages=find_packages(),
     install_requires=[
-        'pandas>=0.21',
-        'numpy>=1.16',
-        'astor>=0.5',
+        'pandas>=0.21, <0.24',
+        'numpy>=1.15',
         'numba>=0.35',
         'numexpr>=2.6',
-        'six>=1.10'
+        'six>=1.10',
+        'matplotlib>=3.0'
     ]
 )
