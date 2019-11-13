8a56f0b6ab0975f5d4ec18aaa114e90ef5f1d2ba
PeterKucirek <peter.kucirek@wsp.com>
2019-11-13 16:24:30 -0500

    Major refactor of the logging module to allow for more formatting styles, including a JSON serialization option

--- balsa/__init__.py
+++ balsa/__init__.py
@@ -1,3 +1,2 @@
 from .routines import *
-from .logging import get_model_logger, init_root, log_to_file, ModelLogger
 from .configuration import Config, ConfigParseError, ConfigSpecificationError, ConfigTypeError
--- balsa/logging.py
+++ balsa/logging.py
@@ -4,34 +4,32 @@ import logging
 import sys
 from contextlib import contextmanager
 import traceback as tb
+import six
+from json import dumps as json_to_str
+from enum import Enum
 
-_DEFAULT_FMT = "%(asctime)s %(levelname)s %(name)s ➔ %(message)s"
-_COLOUR_CODES = {'red': 31, 'green': 32, 'gold': 33, 'yellow': 33, 'blue': 34}
+# region Constants
 
-# region Custom levels
-TIP_LEVEL = 21  # Important log info
-logging.addLevelName(TIP_LEVEL, 'TIP')
+_FMT_STRING = "%(asctime)s %(levelname)s %(name)s {arrow} %(message)s"
+_UNC_ARROW = "➔"
+_ASCII_ARROW = "->"
+_TIP_LEVEL = 21  # Important log info
+_REPORT_LEVEL = 19  # Model results
+_FANCY_FORMAT = "FANCY"
+_BASIC_FORMAT = "BASIC"
+_JSON_FORMAT = "JSON"
 
-REPORT_LEVEL = 19  # Model results
-logging.addLevelName(REPORT_LEVEL, 'REPORT')
+
+class LogFormats(Enum):
+
+    FANCY = _FANCY_FORMAT
+    BASIC = _BASIC_FORMAT
+    JSON = _JSON_FORMAT
+
+
 # endregion
 
 # region Classes
-
-
-class _SetFilter(object):
-
-    def __init__(self):
-        self._excluded = set()
-
-    def filter(self, record):
-        return record.levelno not in self._excluded
-
-    def exclude(self, level):
-        self._excluded.add(level)
-
-    def include(self, level):
-        self._excluded.remove(level)
 
 
 class _RangeFilter(object):
@@ -46,10 +44,14 @@ class _RangeFilter(object):
 
 class _SwitchFormatter(logging.Formatter):
 
-    def __init__(self, default_format):
+    def __init__(self, default_format, level_formats):
         super(_SwitchFormatter, self).__init__()
-        self._default = logging.Formatter(default_format)
-        self._formats = {}
+
+        def make_formatter(item):
+            return logging.Formatter(item) if isinstance(item, str) else item
+
+        self._default = make_formatter(default_format)
+        self._formats = {lvl: make_formatter(f) for lvl, f in six.iteritems(level_formats)}
 
     def format(self, record):
         level = record.levelno
@@ -57,14 +59,16 @@ class _SwitchFormatter(logging.Formatter):
             return self._formats[level].format(record)
         return self._default.format(record)
 
-    def add(self, level, fmt):
-        self._formats[level] = logging.Formatter(fmt)
 
-    def change_default(self, fmt):
-        self._default = logging.Formatter(fmt)
+class _JsonFormatter(logging.Formatter):
 
-    def clear(self):
-        self._formats.clear()
+    def format(self, record):
+        keys = ["levelname", "name", "msg", "created"]
+        to_json = {key: getattr(record, key) for key in keys}
+
+        to_json['asctime'] = self.formatTime(record)
+
+        return json_to_str(to_json)
 
 
 class ModelLogger(logging.Logger):
@@ -75,110 +79,107 @@ class ModelLogger(logging.Logger):
     def report(self, msg, *args, **kwargs):
         """Report useful model statistics or results to the user. Distinct from ``.info()`` which provides status
         information. Printed in green when colours are available."""
-        self.log(REPORT_LEVEL, msg, *args, **kwargs)
+        self.log(_REPORT_LEVEL, msg, *args, **kwargs)
 
     def tip(self, msg, *args, **kwargs):
         """Provide a more significant status statement (e.g. new section of the model). Similar to ``.info()``, but
         more emphasized. Printed in blue when colours are available."""
-        self.log(TIP_LEVEL, msg, *args, **kwargs)
-
-    def pipe(self, msg, levelno=0, levelname=None, asctime=None, name=None):
-        """Turn a string message into a logging statement. Designed to work with stdout when working with
-        sub-processes."""
-        d = {"msg": msg}
-        if levelno is not None: d['levelno'] = levelno
-        if levelname is not None: d['levelname'] = levelname
-        if asctime is not None: d['asctime'] = asctime
-        if name is not None: d['name'] = name
-        else: d['name'] = self.name
-
-        rec = logging.makeLogRecord(d)
-        self.handle(rec)
+        self.log(_TIP_LEVEL, msg, *args, **kwargs)
 
 # endregion
 
-# region Global variables & functions
 
-
-def set_console_format(fmt: str, level: int=None, colour: str=None):
-    if colour is not None:
-        code = _COLOUR_CODES[colour.lower()]
-        fmt = "\x1b[{code}m{fmt}\x1b[0m".format(code=code, fmt=fmt)
-
-    if level is not None:
-        _CONSOLE_FORMATTER.add(level, fmt)
-    else:
-        _CONSOLE_FORMATTER.clear()
-        _CONSOLE_FORMATTER.change_default(fmt)
-
-
-def exclude_console_level(level):
-    _CONSOLE_FILTER.exclude(level)
-
-
-def include_console_level(level):
-    _CONSOLE_FILTER.include(level)
-
-
-_CONSOLE_FILTER = _SetFilter()
-_CONSOLE_FORMATTER = _SwitchFormatter(_DEFAULT_FMT)
-
-_STDOUT_HANDLER = logging.StreamHandler(sys.stdout)
-_STDOUT_HANDLER.addFilter(_CONSOLE_FILTER)
-_STDOUT_HANDLER.addFilter(_RangeFilter(0, logging.ERROR - 1))
-_STDOUT_HANDLER.setFormatter(_CONSOLE_FORMATTER)
-
-_STDERR_HANDLER = logging.StreamHandler(sys.stderr)
-_STDERR_HANDLER.addFilter(_CONSOLE_FILTER)
-_STDERR_HANDLER.addFilter(_RangeFilter(logging.ERROR, 100))
-_STDERR_HANDLER.setFormatter(_CONSOLE_FORMATTER)
-
-set_console_format(_DEFAULT_FMT, level=TIP_LEVEL, colour='blue')
-set_console_format(_DEFAULT_FMT, level=REPORT_LEVEL, colour='green')
-set_console_format(_DEFAULT_FMT, level=logging.WARNING, colour='red')
-
-# endregion
-
-# region Loggers
-
-
-def init_root(root_name: str) -> ModelLogger:
-    """
-    Call this function **at the start of a program** to setup Balsa's logging features, including colours.
-
-    Args:
-        root_name: Loggers use dotted namespaces to determine inheritance, which also gets passed into the logging
-            statements. So use a descriptive name for your program, with no spaces in it. Subsequent loggers should
-            include this name, then a dot, and then the model-specific name.
-
-    Returns:
-        ModelLogger: The root ModelLogger object.
-
-    """
+def init_root(root_name, stream_format=LogFormats.FANCY, log_debug=True):
+    logging.addLevelName(_TIP_LEVEL, 'TIP')
+    logging.addLevelName(_REPORT_LEVEL, 'REPORT')
     logging.setLoggerClass(ModelLogger)
-    root = logging.getLogger(root_name)
-    root.propagate = True
-    root.setLevel(1)  # Log everything
+    root_logger = logging.getLogger(root_name)
+    root_logger.propagate = True
+    if log_debug:
+        root_logger.setLevel(1)
+    else:
+        root_logger.setLevel(logging.INFO)
 
-    root.handlers.clear()  # Remove any existing handlers
-    root.addHandler(_STDOUT_HANDLER)
-    root.addHandler(_STDERR_HANDLER)
+    if isinstance(stream_format, str):
+        try:
+            stream_format = LogFormats[stream_format]
+        except KeyError:
+            pass
 
-    return root
+    if stream_format == LogFormats.FANCY:
+        stdout_formatter, stderr_formatter = _prep_fancy_formatter()
+    elif stream_format == LogFormats.BASIC:
+        stdout_formatter, stderr_formatter = _prep_basic_formatter()
+    elif stream_format == LogFormats.JSON:
+        stdout_formatter, stderr_formatter = _prep_json_formatter()
+    else:
+        stdout_formatter, stderr_formatter = _custom_formatter(stream_format)
+
+    stdout_handler = logging.StreamHandler(sys.stdout)
+    stdout_handler.addFilter(_RangeFilter(0, logging.WARNING + 1))
+    stdout_handler.setFormatter(stdout_formatter)
+
+    stderr_handler = logging.StreamHandler(sys.stderr)
+    stderr_handler.addFilter(_RangeFilter(logging.ERROR, 100))
+    stderr_handler.setFormatter(stderr_formatter)
+
+    root_logger.handlers.clear()
+    root_logger.addHandler(stdout_handler)
+    root_logger.addHandler(stderr_handler)
+
+    return root_logger
 
 
-def get_model_logger(name: str) -> ModelLogger:
+def _prep_json_formatter():
+    base_formatter = _JsonFormatter()
+    return base_formatter, base_formatter
+
+
+def _prep_basic_formatter():
+    base_formatter = logging.Formatter(_FMT_STRING.format(arrow=_ASCII_ARROW))
+    return base_formatter, base_formatter
+
+
+def _prep_fancy_formatter():
+    raw_fmt = _FMT_STRING.format(arrow=_UNC_ARROW)
+    fmt_string = ''.join(["\x1b[{colour}m", raw_fmt, "\x1b[0m"])
+
+    info_formatter = logging.Formatter(fmt_string.format(colour=0))  # Black colour
+    report_formatter = logging.Formatter(fmt_string.format(colour=32))  # Green colour
+    tip_formatter = logging.Formatter(fmt_string.format(colour=34))  # Blue colour
+    warn_formatter = logging.Formatter(fmt_string.format(colour=31))  # Red colour
+
+    switch_formatter = _SwitchFormatter(raw_fmt, {
+        logging.INFO: info_formatter, logging.WARNING: warn_formatter, _TIP_LEVEL: tip_formatter,
+        _REPORT_LEVEL: report_formatter
+    })
+
+    error_critical_formatter = logging.Formatter(raw_fmt)
+
+    return switch_formatter, error_critical_formatter
+
+
+def _custom_formatter(fmt):
+    base_formatter = logging.Formatter(fmt)
+    return base_formatter, base_formatter
+
+
+def get_model_logger(name):
     logging.setLoggerClass(ModelLogger)
     return logging.getLogger(name)
 
 
 @contextmanager
-def log_to_file(file_name: str, name, *, append=False):
+def log_to_file(file_name: str, name, append=False, raw_ascii=False):
     root = logging.getLogger(name)
 
     write_mode = 'a' if append else 'w'
     handler = logging.FileHandler(file_name, mode=write_mode, encoding='utf-8')
-    handler.setFormatter(logging.Formatter(_DEFAULT_FMT))
+
+    arrow = _ASCII_ARROW if raw_ascii else _UNC_ARROW
+    fmt_str = _FMT_STRING.format(arrow=arrow)
+    handler.setFormatter(logging.Formatter(fmt_str))
+
     handler.addFilter(_RangeFilter(0, 100))
 
     root.addHandler(handler)
@@ -191,5 +192,3 @@ def log_to_file(file_name: str, name, *, append=False):
         raise
     finally:
         root.removeHandler(handler)
-
-# endregion
